f14163fafa1532ea4a256f9127a4b1dbd0fccd13
yyngfive <chenhye5@outlook.com>
2024-05-20 01:41:08 +0800

    完成鼠标坐标定位

--- select_lane.py
+++ select_lane.py
@@ -4,8 +4,11 @@ import numpy as np
 from ttkbootstrap.constants import *
 from icecream import ic
 
+UPDATE_DELAY = 10
 
-root = ttk.Window()
+root = ttk.Window(
+    size=(500,200)
+)
 
 
 class Img(ttk.Toplevel):
@@ -15,17 +18,28 @@ class Img(ttk.Toplevel):
         self.sw = self.winfo_screenwidth()
         self.sh = self.winfo_screenheight()
         self.img = img
+        self.focus = False
         
         ic(self.sh)
         self.fit_screen()
         self.show_img()
+        self.get_lane()
+        
+    def get_lane(self):
+        self.canvas.bind('<Motion>', self.get_pos)
+        
+    def get_pos(self,event):
+        self.focus = True
+        self.x = event.x * self.resize_coef
+        self.y = event.y * self.resize_coef
+        #ic(event.x,event.y)
     
     def show_img(self):
         width,height = self.resize
         ic(width,height)
-        canvas = ttk.Canvas(self,height=height,width=width)
-        image = canvas.create_image(0,0,anchor='nw',image=self.photo)
-        canvas.pack()
+        self.canvas = ttk.Canvas(self,height=height,width=width)
+        image = self.canvas.create_image(0,0,anchor='nw',image=self.photo)
+        self.canvas.pack()
     
     def fit_screen(self):
         ic(self.img.size)
@@ -38,6 +52,7 @@ class Img(ttk.Toplevel):
         else:
             resize = (coef * self.sh * img_width / img_height,coef * self.sh)
         self.resize = tuple(map(int,resize))
+        self.resize_coef = img_width / self.resize[0]
         ic(self.resize)
         self.img_resized = self.img.resize(self.resize)
         self.photo = ImageTk.PhotoImage(self.img_resized)
@@ -53,7 +68,19 @@ class App(ttk.Frame):
 
         self.file_name = "./test1.tif"
         self.img = Image.open(self.file_name)
+        self.img_x = 0
         self.show_img()
+        self.show_info_bar()
+        
+        
+    def show_info_bar(self):
+        self.info_bar = ttk.Frame(self)
+        self.info_bar.pack(fill=X, pady=1, side=BOTTOM)
+        self.img_pos = ttk.StringVar()
+        self.img_pos.set('')
+        pos_label = ttk.Label(self.info_bar,textvariable=self.img_pos)
+        pos_label.pack()
+        
 
     def create_menubar(self):
         menubar = ttk.Menu(self)
@@ -68,6 +95,18 @@ class App(ttk.Frame):
 
     def show_img(self):
         self.img_window = Img(self,self.img)
+        #self.wait_window(self.img_window)
+        self.after(UPDATE_DELAY,self.update)
+        
+    
+    def update(self):
+        #ic(self.img_window.focus)
+        if(self.img_window.focus):
+            x = self.img_window.x
+            y = self.img_window.y
+            #ic(x)
+            self.img_pos.set(f'x = {int(x)}, y = {int(y)}')
+        self.after(UPDATE_DELAY,self.update)
 
 
 App(root)
