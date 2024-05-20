b545df611b062eb9d7bdcbabad29166f5ec223d3
yyngfive <chenhye5@outlook.com>
2024-05-20 13:50:44 +0800

    实现了矩形绘制

--- select_lane.py
+++ select_lane.py
@@ -4,11 +4,9 @@ import numpy as np
 from ttkbootstrap.constants import *
 from icecream import ic
 
-UPDATE_DELAY = 10
+UPDATE_DELAY = 100
 
-root = ttk.Window(
-    size=(500,200)
-)
+root = ttk.Window(size=(500, 200))
 
 
 class Img(ttk.Toplevel):
@@ -19,39 +17,67 @@ class Img(ttk.Toplevel):
         self.sh = self.winfo_screenheight()
         self.img = img
         self.focus = False
-        
+        self.nw = None
+        self.se = None
+        self.rec = None
+        self.update_id = None
         ic(self.sh)
         self.fit_screen()
         self.show_img()
         self.get_lane()
-        
+
     def get_lane(self):
-        self.canvas.bind('<Motion>', self.get_pos)
-        
-    def get_pos(self,event):
+        self.canvas.bind("<Motion>", self.get_pos)
+        self.canvas.bind("<B1-Motion>", self.set_pos_nw)
+        self.canvas.bind("<ButtonRelease-1>",self.set_pos_se)
+
+    def get_pos(self, event):
         self.focus = True
         self.x = event.x * self.resize_coef
         self.y = event.y * self.resize_coef
-        #ic(event.x,event.y)
+        self.canvas_x = event.x
+        self.canvas_y = event.y
+        # ic(event.x,event.y)
+
+    def set_pos_nw(self,event):
+        
+        self.pos_nw = (event.x,event.y)
+        self.update_id = self.after(UPDATE_DELAY,self.update_rec)
+    
+    def set_pos_se(self,event):
+        
+        self.pos_se = (self.x,self.y)
+        self.pos_nw = None
+        self.after_cancel(self.update_id)
+         
+    
+    def update_rec(self):
+        self.draw_rec()
+        self.update_id = self.after(UPDATE_DELAY,self.update_rec)
+
+    def draw_rec(self):
+        if self.pos_nw:
+            self.canvas.delete(self.rec)
+            self.rec = self.canvas.create_rectangle(self.pos_nw[0],self.pos_nw[1],self.canvas_x,self.canvas_y,fill='',outline='blue',width=2)
     
     def show_img(self):
-        width,height = self.resize
-        ic(width,height)
-        self.canvas = ttk.Canvas(self,height=height,width=width)
-        image = self.canvas.create_image(0,0,anchor='nw',image=self.photo)
+        width, height = self.resize
+        ic(width, height)
+        self.canvas = ttk.Canvas(self, height=height, width=width)
+        image = self.canvas.create_image(0, 0, anchor="nw", image=self.photo)
         self.canvas.pack()
-    
+
     def fit_screen(self):
         ic(self.img.size)
         coef = 0.8
-        img_width,img_height = self.img.size
-        if img_height < self.sh / 2 :
-            resize = (img_width,img_height)
+        img_width, img_height = self.img.size
+        if img_height < self.sh / 2:
+            resize = (img_width, img_height)
         elif img_height < self.sh:
-            resize = (coef * img_width,coef * img_height)
+            resize = (coef * img_width, coef * img_height)
         else:
-            resize = (coef * self.sh * img_width / img_height,coef * self.sh)
-        self.resize = tuple(map(int,resize))
+            resize = (coef * self.sh * img_width / img_height, coef * self.sh)
+        self.resize = tuple(map(int, resize))
         self.resize_coef = img_width / self.resize[0]
         ic(self.resize)
         self.img_resized = self.img.resize(self.resize)
@@ -71,16 +97,14 @@ class App(ttk.Frame):
         self.img_x = 0
         self.show_img()
         self.show_info_bar()
-        
-        
+
     def show_info_bar(self):
         self.info_bar = ttk.Frame(self)
         self.info_bar.pack(fill=X, pady=1, side=BOTTOM)
         self.img_pos = ttk.StringVar()
-        self.img_pos.set('')
-        pos_label = ttk.Label(self.info_bar,textvariable=self.img_pos)
+        self.img_pos.set("")
+        pos_label = ttk.Label(self.info_bar, textvariable=self.img_pos)
         pos_label.pack()
-        
 
     def create_menubar(self):
         menubar = ttk.Menu(self)
@@ -94,19 +118,18 @@ class App(ttk.Frame):
         self.show_img()
 
     def show_img(self):
-        self.img_window = Img(self,self.img)
-        #self.wait_window(self.img_window)
-        self.after(UPDATE_DELAY,self.update)
-        
-    
+        self.img_window = Img(self, self.img)
+        # self.wait_window(self.img_window)
+        self.after(UPDATE_DELAY, self.update)
+
     def update(self):
-        #ic(self.img_window.focus)
-        if(self.img_window.focus):
+        # ic(self.img_window.focus)
+        if self.img_window.focus:
             x = self.img_window.x
             y = self.img_window.y
-            #ic(x)
-            self.img_pos.set(f'x = {int(x)}, y = {int(y)}')
-        self.after(UPDATE_DELAY,self.update)
+            # ic(x)
+            self.img_pos.set(f"x = {int(x)}, y = {int(y)}")
+        self.after(UPDATE_DELAY, self.update)
 
 
 App(root)
